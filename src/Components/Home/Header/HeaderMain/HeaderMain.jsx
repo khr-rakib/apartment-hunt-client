7cf4c8639b5434e0232bde523bc13c626fa2d3d6
kawsar00 <kawsarmahmud358@gmail.com>
2020-11-16 07:38:09 +0600

    header part complete

---
+++ src/Components/Home/Header/HeaderMain/HeaderMain.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './HeaderMain.css'
+
+const HeaderMain = () => {
+  return (
+    <div className="header-container header-bg d-flex justify-content-center align-items-center">
+      <div className="container-fluid row d-flex justify-content-center">
+        <div className="col-md-10 text-center">
+          <h1 className="text-white my-4">FIND YOUR HOUSE RENT</h1>
+          <span><input className="w-50 px-3 py-2 mr-3 input-style" type="text" placeholder="Search..." /></span>
+          <span><input className="btn btn-style text-white px-4 py-2" type="submit" value="Find Now"/></span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderMain;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle'
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
